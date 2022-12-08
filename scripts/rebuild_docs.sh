5dece33529bb0918d146c9df1551928712b9e12a
Artur Barseghyan <artur.barseghyan@gmail.com>
2022-12-08 09:10:18 +0100

    Update rebuild_docs.sh

--- scripts/rebuild_docs.sh
+++ scripts/rebuild_docs.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 ./scripts/uninstall.sh
 ./scripts/install.sh
-rm docs/*.rst
-rm -rf builddocs/
+./scripts/clean_up.sh
 sphinx-apidoc src/faker-file --full -o docs -H 'faker-file' -A 'Artur Barseghyan <artur.barseghyan@gmail.com>' -f -d 20
 cp docs/conf.py.distrib docs/conf.py
