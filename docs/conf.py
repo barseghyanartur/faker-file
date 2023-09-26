b9496377009aea8ad6c95aeb1f068ccedec49d78
Artur Barseghyan <artur.barseghyan@gmail.com>
2023-09-26 21:58:39 +0200

    Up docs

--- docs/conf.py
+++ docs/conf.py
@@ -69,7 +69,7 @@ master_doc = "index"
 
 # General information about the project.
 # project = u'faker-file'
-# copyright = u'2017, Artur Barseghyan <artur.barseghyan@gmail.com>'
+# copyright = u'2022-2023, Artur Barseghyan <artur.barseghyan@gmail.com>'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -120,7 +120,8 @@ pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = "default"
+# html_theme = "default"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
