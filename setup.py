3e26f6d0d9921134ce0741b5dfe1e554c4bb8e5b
crabby-rathbun <crabby.rathbun@gmail.com>
2026-02-12 10:27:15 -0800

    Add Python 3.13 support
    - Add Python 3.13 classifier to setup.py
    - Enable Python 3.13 in GitHub Actions test matrix
    - Add py313 to tox.ini envlist and gh-actions mapping
    - Update pyproject.toml target-version and pythonVersion to 3.13
    - Exclude Python 3.13 + pathy 0.10.3 combination (incompatible)
    
    Fixes #108

--- setup.py
+++ setup.py
@@ -112,6 +112,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
         "Environment :: Web Environment",
         "License :: OSI Approved :: MIT License",
         "Intended Audience :: Developers",
