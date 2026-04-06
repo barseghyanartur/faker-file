57e9931a70e7ced19bf4cb9581065be5531832c8
Artur Barseghyan <artur.barseghyan@gmail.com>
2026-04-07 01:48:57 +0200

    Initial, still needs to be properly reviewdd

--- conftest.py
+++ conftest.py
@@ -1,1 +1,8 @@
+import pytest
 from faker_file.tests._conftest import *  # noqa
+
+
+@pytest.fixture()
+def my_fixture() -> None:
+    """My fixture used just as an example, since referred in agentic docs."""
+    return None
